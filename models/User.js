68fe2484a9b288a85bd9012d8f5b37f00cc60d23
42azimut <azimutuniverse@gmail.com>
2021-04-30 17:36:37 +0900

    #5 handleErrors. validation, error message

--- models/User.js
+++ models/User.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose');
+const { isEmail } = require('validator');
+
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: true,
+    required: [true, 'Please Enter an Email'],
     unique: true,
     lowercase: true,
+    validate: [isEmail, 'Please Enter a valid Email']
   },
   password: {
     type: String,
-    required: true,
-    minlength: 4,
+    required: [true, 'Please Enter a valid PassWord'],
+    minlength: [4, 'Minimum Password length is 4 characters'],
   },
 })
 
+// fire a function after doc saved to db
+userSchema.post('save', function (doc, next) {
+  console.log('new user was created & saved', doc);
+})
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
