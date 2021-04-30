935fcc64793ad596daf50f7042fdbeefff35399b
42azimut <azimutuniverse@gmail.com>
2021-04-30 15:18:34 +0900

    #2 auth routes and controllers done

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 const app = express();
 
@@ -12,13 +16,16 @@ app.set('view engine', 'ejs');
 // database connection
 //const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
 const dbURI = 'mongodb://azimut:1111@localhost:27017/admin';
-mongoose.connect(dbURI, { dbName: 'node-auth', useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+mongoose.connect(dbURI, { 
+  dbName: 'node-auth', 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true, 
+  useCreateIndex:true 
+})
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
-
-
-
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
