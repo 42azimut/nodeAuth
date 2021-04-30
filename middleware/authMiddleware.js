80d43e872e8b91302f1ae539eb8d33164ec6540b
42azimut <azimutuniverse@gmail.com>
2021-05-01 08:33:29 +0900

    #17 checking the current User

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
 
@@ -19,4 +20,28 @@ const requireAuth = (req, res, next) => {
     res.redirect('/login');
   }
 }
-module.exports = { requireAuth };
+
+// check current user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'azimut secret', async (err, decodedToken) => {
+      if (err) {
+        console.log(er.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    })
+  } else {
+    res.locals.user = null;
+    next();
+  }
+}
+
+module.exports = { requireAuth, checkUser };
