1a47be3183d7aa8afda88448e74a1ceec3ce7758
42azimut <azimutuniverse@gmail.com>
2021-05-01 00:28:06 +0900

    #13 Logging Users in part1

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,12 +55,16 @@ module.exports.signup_post = async (req, res) => {
     const errors = handleErrors(err);
     res.status(400).json({ errors });
   }
- 
 }
 
 module.exports.login_post = async (req, res) => {
   const { email, password } = req.body;
 
-  console.log(email, password);
-  res.send('user login');
+  try {
+    const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    res.status(400).json({});
+  }
+
 }
