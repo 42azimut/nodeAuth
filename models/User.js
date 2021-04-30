4ec1c8c4f03c911c706f9d6aae46ca5c59e9c66e
42azimut <azimutuniverse@gmail.com>
2021-04-30 18:19:08 +0900

    #6 mongoose hooks _ userSchema.pre, userSchema.post

--- models/User.js
+++ models/User.js
@@ -20,6 +20,12 @@ const userSchema = new mongoose.Schema({
 // fire a function after doc saved to db
 userSchema.post('save', function (doc, next) {
   console.log('new user was created & saved', doc);
+  next();
+});
+
+userSchema.pre('save', function (next) {
+  console.log('user about to be created & saved', this);
+  next();
 })
 const User = mongoose.model('user', userSchema);
 
